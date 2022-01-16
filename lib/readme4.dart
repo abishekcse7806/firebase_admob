af687b32c5828e93774bef0dad5c74334ab7ed99
Abishek CSE <abishekcse78@gmail.com>
2022-01-16 20:23:37 +0530

    Commit

---
+++ lib/readme4.dart
@@ -0,0 +1,59 @@
+MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+  keywords: <String>['flutterio', 'beautiful apps'],
+  contentUrl: 'https://flutter.io',
+  birthday: DateTime.now(),
+  childDirected: false,
+  designedForFamilies: false,
+  gender:
+      MobileAdGender.male, // or MobileAdGender.female, MobileAdGender.unknown
+  testDevices: <String>[], // Android emulators are considered test devices
+);
+
+BannerAd myBanner = BannerAd(
+  // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+  // https://developers.google.com/admob/android/test-ads
+  // https://developers.google.com/admob/ios/test-ads
+  adUnitId: BannerAd.testAdUnitId,
+  size: AdSize.smartBanner,
+  targetingInfo: targetingInfo,
+  listener: (MobileAdEvent event) {
+    print("BannerAd event is $event");
+  },
+);
+
+InterstitialAd myInterstitial = InterstitialAd(
+  // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+  // https://developers.google.com/admob/android/test-ads
+  // https://developers.google.com/admob/ios/test-ads
+  adUnitId: InterstitialAd.testAdUnitId,
+  targetingInfo: targetingInfo,
+  listener: (MobileAdEvent event) {
+    print("InterstitialAd event is $event");
+  },
+);
+
+///Ads must be loaded before they're shown.
+
+
+
+myBanner
+// typically this happens well before the ad is shown
+..load()
+..show(
+// Positions the banner ad 60 pixels from the bottom of the screen
+anchorOffset: 60.0,
+// Positions the banner ad 10 pixels from the center of the screen to the right
+horizontalCenterOffset: 10.0,
+// Banner Position
+anchorType: AnchorType.bottom,
+);
+
+
+
+myInterstitial
+..load()
+..show(
+anchorType: AnchorType.bottom,
+anchorOffset: 0.0,
+horizontalCenterOffset: 0.0,
+);
